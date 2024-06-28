b5f3c3c5688145a255d36af15a0e7a0df0a65cc6
Jitin Rathi <jitinrathi910949@gmail.com>
2024-06-28 16:08:28 +0530

    tailwind new option

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  content: ["./src/**/*.{astro,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
+export default {
+  darkMode: ["class"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "sans": ["Atkinson", ...defaultTheme.fontFamily.sans],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: "full",
+          },
+        },
+      },
+    }
+  }
+};
